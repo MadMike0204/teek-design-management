29c4c7d777de19eb788a48967d70a34311597d43
Ru1h1015 <chenruihang666@gmail.com>
2025-12-19 18:52:37 +0800

    默认浅色--之前默认深色了

--- src/common/config/service/index.ts
+++ src/common/config/service/index.ts
@@ -1,13 +1,16 @@
 import type { ServiceConfig } from "./types";
 import { defineServiceConfig } from "./override-config";
+import { GlobalThemeEnum } from "@/common/enums";
 
 export { defaultServiceConfig } from "./base-config";
 
 // 可以在这里覆盖框架默认的配置
-//覆盖logo
 const overrideServiceConfig: DeepPartial<ServiceConfig> = {
   logo: {
     enable: false
+  },
+  theme: {
+    globalThemeMode: GlobalThemeEnum.Light
   }
 };
 
